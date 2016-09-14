63fb7ce059f26864301edc6f808c33da156622a1
Kevin Lewis <phazonoverload@users.noreply.github.com>
2016-09-14 13:36:52 +0100

    deploy from ship

--- js/script.js
+++ js/script.js
@@ -4,4 +4,27 @@ $(document).ready(function() {
 		$(this).siblings("p").toggle();
 		$(this).parent().toggleClass("dropped");
 	});
+
+
+	var iframeLink = "https://www.youtube.com/embed/s2J4J4uJjk0";
+
+	$(".modal-trigger").click(function() {
+		$(".modal").removeClass("hidden");
+		$(".modal iframe").attr("src", iframeLink);
+	});
+
+	function hideModal() {
+		$(".modal").addClass("hidden");
+		$(".modal iframe").attr("src", "");
+	}
+
+	$(".modal .page-wipe").click(function(){
+		hideModal()
+	});
+
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			hideModal();
+		}
+	});
 });
